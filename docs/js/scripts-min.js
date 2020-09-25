afd61589ed8475486f4c40eb5f12ec3339181079
DorianDesings <dorianoriginaldesings@gmail.com>
2020-09-26 01:19:27 +0200

    mobile menu implemented

--- docs/js/scripts-min.js
+++ docs/js/scripts-min.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";var topBar=document.getElementById("top-bar"),mainNav=document.getElementById("main-nav"),iconMenu=document.getElementById("icon-menu");iconMenu.addEventListener("click",(function(){topBar.classList.contains("top-bar--show")?(iconMenu.src="/assets/images/icon-hamburger.svg",mainNav.classList.remove("main-nav--show"),topBar.classList.remove("top-bar--show")):(iconMenu.src="/assets/images/icon-close.svg",mainNav.classList.add("main-nav--show"),topBar.classList.add("top-bar--show"))}));
