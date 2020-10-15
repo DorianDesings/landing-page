5a02eb652529af336140dde608b8f43d9fd84e9e
rr69sport <richardramsua@gmail.com>
2020-10-15 16:59:43 -0300

    apply change by eslint and editorconfig

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,10 +1,14 @@
 const accordion = document.getElementById('accordion');
 const accordionTexts = [...document.querySelectorAll('.accordion__text')];
 
-accordion.addEventListener('click', e => {
+accordion.addEventListener('click', (e) => {
+
   const targetClass = e.target.classList;
   if (targetClass.contains('accordion__title')) {
+
     e.target.nextElementSibling.classList.toggle('accordion__text--active');
     e.target.classList.toggle('accordion__title--active');
-  }
+
+}
+
 });
--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,19 @@ const mainNav = document.getElementById('main-nav');
 const iconMenu = document.getElementById('icon-menu');
 
 iconMenu.addEventListener('click', () => {
+
   if (topBar.classList.contains('top-bar--show')) {
+
     iconMenu.src = '/assets/images/icon-hamburger.svg';
     mainNav.classList.remove('main-nav--show');
     topBar.classList.remove('top-bar--show');
-  } else {
+
+} else {
+
     iconMenu.src = '/assets/images/icon-close.svg';
     mainNav.classList.add('main-nav--show');
     topBar.classList.add('top-bar--show');
-  }
+
+}
+
 });
