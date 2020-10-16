24bd075bd1d111a2846bec3e6429b8f526fcf1be
rr69sport <richardramsua@gmail.com>
2020-10-16 17:52:21 -0300

    images folder restructuring

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -9,10 +9,10 @@ tabs.addEventListener('click', (e) => {
 
         tabsItem.forEach((item, idx) => {
 
-        item.classList.remove('tabs__item--active');
-        tabsContent[idx].classList.remove('tabs__content--active');
+            item.classList.remove('tabs__item--active');
+            tabsContent[idx].classList.remove('tabs__content--active');
 
-    });
+        });
         e.target.classList.add('tabs__item--active');
         tabsContent[index].classList.add('tabs__content--active');
 
